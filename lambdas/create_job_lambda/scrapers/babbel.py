0447af9aa9950119d498d27c5e56ed93c4678011
L-Richter <l.r.t@web.de>
2017-10-30 01:47:16 +0100

    small fixes in scrapers

--- lambdas/create_job_lambda/scrapers/babbel.py
+++ lambdas/create_job_lambda/scrapers/babbel.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def get_jobs(self):
+def get_jobs():
     api_url = 'https://api.greenhouse.io/v1/boards/babbel/departments'
     response = requests.get(api_url)
     response.raise_for_status()
--- lambdas/create_job_lambda/scrapers/fliit.py
+++ lambdas/create_job_lambda/scrapers/fliit.py
@@ -1,2 +1,3 @@
 def get_jobs():
     return []
+
