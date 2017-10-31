4e899fb787910f4b4a8e27339a6c56e431d14119
L-Richter <l.r.t@web.de>
2017-10-31 21:01:03 +0100

    added more job details

--- lambdas/get_job_details_lambda/scrapers/babbel.py
+++ lambdas/get_job_details_lambda/scrapers/babbel.py
@@ -1,17 +1,24 @@
 import requests
 import json
+import hashlib
+from collections import defaultdict
 
 def get_details(job_resource):
     api_url = f'https://api.greenhouse.io/v1/boards/babbel/jobs/{job_resource}'
     response = requests.get(api_url)
     response.raise_for_status()
-    job = json.loads(response.text)
+    job = defaultdict(str, json.loads(response.text))
     job_details = {}
     job_details['company_name'] = 'Babbel'
-    job_details['natural_id'] = job_resource
+    job_details['natural_id'] = hash_string(job_resource)
     job_details['job_title'] = job['title']
     job_details['job_description'] = job['content']
+    job_details['job_description_id'] = hash_string(job['content'])
     job_details['departments'] = [dep['name'] for dep in job['departments']]
     job_details['location'] = job['location']['name']
     return job_details
+
+
+def hash_string(s):
+    return hashlib.md5(s.encode('utf-8')).hexdigest()
 
