2868508a669b190bb65b66cbb49ee6c5d943b059
L-Richter <l.r.t@web.de>
2017-11-05 14:38:27 +0100

    fix typo

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -9,8 +9,8 @@ resource "aws_db_instance" "postgresql" {
   name                       = "pg-descriptions"
   password                   = "${var.database_password}"
   username                   = "${var.database_username}"
-  backup_retention_period    = "$4"
-  backup_window              = "$06:00-07:00"
+  backup_retention_period    = "4"
+  backup_window              = "06:00-07:00"
   multi_az                   = "false"
   port                       = "5432"
 }
