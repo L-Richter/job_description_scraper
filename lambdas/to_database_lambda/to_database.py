c7a4bc7521fb7f2f769ab5d4ebffcd048f38c89a
L-Richter <l.r.t@web.de>
2017-11-19 17:13:12 +0100

    add to database lambda

---
+++ lambdas/to_database_lambda/to_database.py
@@ -0,0 +1,74 @@
+import boto3
+import os
+import logging
+import json
+import psycopg2
+from collections import defaultdict
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info(f'start pushing {event} to db')
+    db_host = os.environ['DB_HOST']
+    db_username = os.environ['DB_USERNAME']
+    db_password = os.environ['DB_PASSWORD']
+    db_name = os.environ['DB_NAME']
+
+    source_bucket_name = event['Records'][0]['s3']['bucket']['name']
+    source_key = event['Records'][0]['s3']['object']['key']
+    s3 = boto3.resource('s3')
+    source_object = s3.Object(source_bucket_name, source_key)
+    job = json.loads(source_object.get()['Body'].read().decode('utf-8'))
+    job = defaultdict(lambda : None, job)
+    job['job_details'] = defaultdict(lambda : None, job['job_details'])
+    conn = psycopg2.connect(host=db_host,
+                            user=db_username,
+                            password=db_password,
+                            dbname=db_name)
+    cur = conn.cursor()
+    qry = """INSERT INTO job_descriptions_staging (
+            	created_at,
+	            trigger_date,
+	            company,
+	            company_name,
+	            version,
+	            storage_key,
+	            natural_id,
+	            job_title,
+	            job_description_hash,
+	            location,
+	            department)
+	          VALUES (
+	            (%created_at),
+	            (%trigger_date),
+	            (%company),
+	            (%company_name),
+	            (%version),
+	            (%storage_key),
+	            (%natural_id),
+	            (%job_title),
+	            (%job_description_hash),
+	            (%location),
+	            (%department));"""
+	departments = job['job_details']['department']
+	department = None
+	if departments:
+	    department = ''.join(departments)
+    values = {  'created_at': job['created_at'],
+                'trigger_date': job['date'],
+                'company': job['company'],
+                'company_name': job['job_details']['company_name'],
+                'version': job['version'],
+                'storage_key': source_key,
+                'natural_id': job['job_details']['natural_id'],
+                'job_title': job['job_details']['job_title'],
+                'job_description_hash': job['job_details']['job_description_id'],
+                'location': job['job_details']['location'],
+                'department': department
+              }
+    cur.execute(qry, values)
+    conn.commit()
+    cur.close()
+    conn.close()
+
